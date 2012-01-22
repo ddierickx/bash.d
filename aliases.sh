cdfdba45a056878977338d2ac4ee2bbd11fa8153
Dominique Dierickx <d.dierickx@gmail.com>
2012-01-22 14:45:25 +0100

    Renamed up to .., added df and du aliases

--- aliases.sh
+++ aliases.sh
@@ -7,9 +7,11 @@ function mkcd_f() { mkdir -p "$@" && cd "$@"; }
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias mkcd='mkcd_f'
-alias up='cd ..'
-alias upp='up;up'
-alias uppp='up;up;up'
+alias ..='cd ..'
+alias ...='..;..'
+alias df='df -h'
+alias df='du -h'
+
 
 # single screen, mute sound...
 alias goto-meeting='disper -s && amixer set Master mute'
